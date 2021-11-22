87d2f2bbc3858505e5d19bddef52e106741e5527
Coos Baakman <cbaakman@radboudumc.nl>
2021-11-22 15:56:31 +0100

    graph object

---
+++ deeprank_gnn/models/graph.py
@@ -0,0 +1,12 @@
+import networkx
+
+
+def Graph(networkx.Graph):
+    def __init__(self, id_)
+        networkx.Graph.__init__(self)
+
+        self._id = id_
+
+    @property
+    def id(self):
+        return self._id
