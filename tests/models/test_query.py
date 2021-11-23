d8146ab8b936d7c0d230f25134731f3675b72209
Coos Baakman <cbaakman@radboudumc.nl>
2021-11-23 13:44:51 +0100

    also be sure that the hdf5 file can be loaded into torch

--- tests/models/test_query.py
+++ tests/models/test_query.py
@@ -7,6 +7,7 @@ from deeprank_gnn.models.query import ProteinProteinInterfaceResidueQuery
 from deeprank_gnn.models.environment import Environment
 from deeprank_gnn.domain.feature import FEATURENAME_POSITION, FEATURENAME_EDGEDISTANCE
 from deeprank_gnn.tools.graph import graph_to_hdf5
+from deeprank_gnn.DataSet import HDF5DataSet
 
 
 def test_interface_graph():
@@ -40,5 +41,16 @@ def test_interface_graph():
             assert entry_group["edge_index"].shape[0] > 0, "no edge indices"
             assert entry_group["edge_data/dist"][()].shape[0] == entry_group["edge_index"].shape[0], "not enough edge distances"
 
+            count_edges_hdf5 = entry_group["edge_index"].shape[0]
+
+        dataset = HDF5DataSet(database=tmp_path)
+        torch_data_entry = dataset[0]
+
+        # expecting twice as many edges, because torch is directional
+        count_edges_torch = torch_data_entry.edge_index.shape[1]
+        assert count_edges_torch == 2 * count_edges_hdf5, "got {} edges in output data, hdf5 has {}".format(count_edges_torch, count_edges_hdf5)
+
+        count_edge_features_torch = torch_data_entry.edge_attr.shape[0]
+        assert count_edge_features_torch == count_edges_torch, "got {} edge feature sets, but {} edge indices".format(count_edge_features_torch, count_edges_torch)
     finally:
         os.remove(tmp_path)
