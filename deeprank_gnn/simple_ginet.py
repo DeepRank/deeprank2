62d2ea0ca465bf9911e25dcd60244b2a7d641c55
Coos Baakman <cbaakman@radboudumc.nl>
2021-12-31 12:47:11 +0100

    make the convolutional layer output a set of zeros when there are no edges

--- deeprank_gnn/simple_ginet.py
+++ deeprank_gnn/simple_ginet.py
@@ -17,19 +17,26 @@ class SimpleGiMessageLayer(Module):
 
         super(SimpleGiMessageLayer, self).__init__()
 
-        edge_output_size = count_edge_features
+        self._node_output_size = node_output_size
+        self._edge_output_size = count_edge_features
 
-        self._fn = Linear(count_node_features, node_output_size)
+        self._fn = Linear(count_node_features, self._node_output_size)
         uniform(count_node_features, self._fn.weight)
 
-        self._fe = Linear(count_edge_features, edge_output_size)
+        self._fe = Linear(count_edge_features, self._edge_output_size)
         uniform(count_edge_features, self._fe.weight)
 
-        attention_input_size = 2 * node_output_size + edge_output_size
-        self._fa = Linear(attention_input_size, 1)
-        uniform(attention_input_size, self._fa.weight)
+        self._attention_input_size = 2 * self._node_output_size + self._edge_output_size
+        self._fa = Linear(self._attention_input_size, 1)
+        uniform(self._attention_input_size, self._fa.weight)
 
     def forward(self, node_features, edge_node_indices, edge_features):
+
+        if edge_node_indices.shape[0] != 2:
+
+            # empty list of edges
+
+            return torch.zeros((node_features.shape[0], self._node_output_size))
 
         node0_indices, node1_indices = edge_node_indices
 
@@ -67,21 +74,30 @@ class SimpleGiNetwork(Module):
 
         super(SimpleGiNetwork, self).__init__()
 
-        self._message_layer1 = SimpleGiMessageLayer(input_shape, input_shape_edge, 16)
-        self._message_layer2 = SimpleGiMessageLayer(16, input_shape_edge, 32)
+        self._internal_message_layer1 = SimpleGiMessageLayer(input_shape, input_shape_edge, 16)
+        self._internal_message_layer2 = SimpleGiMessageLayer(16, input_shape_edge, 32)
 
-        self._fc1 = Linear(32, 128)
-        uniform(32, self._fc1.weight)
+        self._edge_message_layer1 = SimpleGiMessageLayer(input_shape, input_shape_edge, 16)
+        self._edge_message_layer2 = SimpleGiMessageLayer(16, input_shape_edge, 32)
+
+        self._fc1 = Linear(64, 128)
+        uniform(64, self._fc1.weight)
 
         self._fc2 = Linear(128, output_shape)
         uniform(128, self._fc2.weight)
 
     def forward(self, data):
 
-        updated1 = relu(self._message_layer1(data.x, data.internal_edge_index, data.internal_edge_attr))
-        updated2 = relu(self._message_layer2(updated1, data.internal_edge_index, data.internal_edge_attr))
+        internal_updated1 = relu(self._internal_message_layer1(data.x, data.internal_edge_index, data.internal_edge_attr))
+        internal_updated2 = relu(self._internal_message_layer2(internal_updated1, data.internal_edge_index, data.internal_edge_attr))
 
-        updated_per_graph = scatter_mean(updated2, data.batch, dim=0)
+        edge_updated1 = relu(self._edge_message_layer1(data.x, data.edge_index, data.edge_attr))
+        edge_updated2 = relu(self._edge_message_layer2(edge_updated1, data.edge_index, data.edge_attr))
+
+        internal_updated_per_graph = scatter_mean(internal_updated2, data.batch, dim=0)
+        edge_updated_per_graph = scatter_mean(edge_updated2, data.batch, dim=0)
+
+        updated_per_graph = torch.cat([internal_updated_per_graph, edge_updated_per_graph], dim=1)
 
         output1 = dropout(relu(self._fc1(updated_per_graph)), 0.4, training=self.training)
 
