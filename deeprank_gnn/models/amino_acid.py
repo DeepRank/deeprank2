7d8fddafb024dd156c4a635ad8cd18109d11a413
Coos Baakman <cbaakman@radboudumc.nl>
2022-02-04 15:32:27 +0100

    adds the sizes to the amino acids

--- deeprank_gnn/models/amino_acid.py
+++ deeprank_gnn/models/amino_acid.py
@@ -4,7 +4,7 @@ import numpy
 class AminoAcid:
     "a value to represent one of the amino acids"
 
-    def __init__(self, name, three_letter_code, one_letter_code, charge=None, polarity=None, index=None):
+    def __init__(self, name, three_letter_code, one_letter_code, charge=None, polarity=None, size=None, index=None):
         """
             Args:
                 name(str): unique name for the amino acid
@@ -12,6 +12,7 @@ class AminoAcid:
                 one_letter_code(str): letter of the amino acid, as in fasta
                 charge(float, optional): the charge property of the amino acid
                 polarity(deeprank polarity enum, optional): the polarity property of the amino acid
+                size(int, optional): the number of heavy atoms in the side chain
                 index(int, optional): the rank of the amino acid, used for computing one-hot encoding
         """
 
