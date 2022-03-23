1af1c8a23ff44d16b983cc8e58c99d3b7732f007
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2022-03-23 10:07:42 +0100

    Use old order for classes in models/structure.py

--- deeprank_gnn/models/structure.py
+++ deeprank_gnn/models/structure.py
@@ -8,6 +8,171 @@ from deeprank_gnn.models.amino_acid import AminoAcid
 from deeprank_gnn.models.conservation import ConservationRow
 
 
+class Structure:
+    "represents one entire pdb structure"
+
+    def __init__(self, id_: str = None):
+        """
+            Args:
+                id_(str): an unique identifier for this structure, can be the pdb accession code.
+        """
+        self._id = id_
+        self._chains = {}
+
+    def __eq__(self, other) -> bool:
+        return type(self) == type(other) and self._id == other._id
+
+    def __hash__(self) -> hash:
+        return hash(self._id)
+
+    def __repr__(self) -> str:
+        return self._id
+
+    def get_chain(self, chain_id: str):
+        return self._chains[chain_id]
+
+    def add_chain(self, chain):
+        if chain.id in self._chains:
+            raise ValueError("duplicate chain: {}".format(chain.id))
+
+        self._chains[chain.id] = chain
+
+    @property
+    def chains(self):
+        return list(self._chains.values())
+
+    def get_atoms(self):
+        "shortcut to list all atoms in this structure"
+        atoms = []
+        for chain in self._chains.values():
+            for residue in chain.residues:
+                atoms.extend(residue.atoms)
+
+        return atoms
+
+
+class Chain:
+    "represents one pdb chain"
+
+    def __init__(self, model: Structure, id_: str):
+        """
+        Args:
+            model(deeprank structure object): the model that this chain is part of
+            id_(str): the pdb identifier of this chain
+        """
+
+        self._model = model
+        self._id = id_
+        self._residues = []
+        self._pssm = None  # pssm is per chain
+
+    @property
+    def model(self):
+        return self._model
+
+    @property
+    def pssm(self) -> ConservationRow:
+        return self._pssm
+
+    @pssm.setter
+    def pssm(self, pssm: ConservationRow):
+        self._pssm = pssm
+
+    def add_residue(self, residue):
+        self._residues.append(residue)
+
+    @property
+    def id(self) -> str:
+        return self._id
+
+    @property
+    def residues(self):
+        return self._residues
+
+    def __eq__(self, other) -> bool:
+        return type(self) == type(other) and self._model == other._model and self._id == other._id
+
+    def __hash__(self) -> hash:
+        return hash((self._model, self._id))
+
+    def __repr__(self) -> str:
+        return "{} {}".format(self._model, self._id)
+
+
+class Residue:
+    "represents a pdb residue"
+
+    def __init__(self, chain: Chain, number: int, amino_acid: AminoAcid = None, insertion_code: str = None):
+        """
+        Args:
+            chain(deeprank chain object): the chain that this residue belongs to
+            number(int): the residue number
+            amino_acid(deeprank amino acid, optional): the residue's amino acid (if it's part of a protein)
+            insertion_code(str, optional): the pdb insertion code, if any
+        """
+
+        self._chain = chain
+        self._number = number
+        self._amino_acid = amino_acid
+        self._insertion_code = insertion_code
+        self._atoms = []
+
+    def __eq__(self, other) -> bool:
+        return type(self) == type(other) and \
+            self._chain == other._chain and \
+            self._number == other._number and \
+            self._insertion_code == other._insertion_code
+
+    def __hash__(self) -> hash:
+        return hash((self._chain, self._number, self._insertion_code))
+
+    def get_pssm(self) -> ConservationRow:
+        """ if the residue's chain has pssm info linked to it,
+            then return the part that belongs to this residue
+        """
+
+        pssm = self._chain.pssm
+        if pssm is None:
+            raise ValueError("pssm not set on {}".format(self._chain))
+
+        return pssm[self]
+
+    @property
+    def number(self) -> int:
+        return self._number
+
+    @property
+    def chain(self):
+        return self._chain
+
+    @property
+    def amino_acid(self) -> AminoAcid:
+        return self._amino_acid
+
+    @property
+    def atoms(self):
+        return self._atoms
+
+    @property
+    def number_string(self) -> str:
+        "contains both the number and the insertion code (if any)"
+
+        if self._insertion_code is not None:
+            return "{}{}".format(self._number, self._insertion_code)
+        else:
+            return str(self._number)
+
+    @property
+    def insertion_code(self) -> str:
+        return self._insertion_code
+
+    def add_atom(self, atom):
+        self._atoms.append(atom)
+
+    def __repr__(self) -> str:
+        return "{} {}".format(self._chain, self.number_string)
+
+
 class AtomicElement(Enum):
     "value to represent the type of pdb atoms"
 
@@ -28,7 +193,7 @@ class AtomicElement(Enum):
 class Atom:
     "represents a pdb atom"
 
-    def __init__(self, residue, name: str, element: AtomicElement, position: numpy.array, occupancy: float):
+    def __init__(self, residue: Residue, name: str, element: AtomicElement, position: numpy.array, occupancy: float):
         """
             Args:
                 residue(deeprank residue object): the residue that this atom belongs to
@@ -79,168 +244,3 @@ class Atom:
     @property
     def residue(self):
         return self._residue
-
-
-class Residue:
-    "represents a pdb residue"
-
-    def __init__(self, chain, number: int, amino_acid: AminoAcid = None, insertion_code: str = None):
-        """
-        Args:
-            chain(deeprank chain object): the chain that this residue belongs to
-            number(int): the residue number
-            amino_acid(deeprank amino acid, optional): the residue's amino acid (if it's part of a protein)
-            insertion_code(str, optional): the pdb insertion code, if any
-        """
-
-        self._chain = chain
-        self._number = number
-        self._amino_acid = amino_acid
-        self._insertion_code = insertion_code
-        self._atoms = []
-
-    def __eq__(self, other) -> bool:
-        return type(self) == type(other) and \
-            self._chain == other._chain and \
-            self._number == other._number and \
-            self._insertion_code == other._insertion_code
-
-    def __hash__(self) -> hash:
-        return hash((self._chain, self._number, self._insertion_code))
-
-    def get_pssm(self) -> ConservationRow:
-        """ if the residue's chain has pssm info linked to it,
-            then return the part that belongs to this residue
-        """
-
-        pssm = self._chain.pssm
-        if pssm is None:
-            raise ValueError("pssm not set on {}".format(self._chain))
-
-        return pssm[self]
-
-    @property
-    def number(self) -> int:
-        return self._number
-
-    @property
-    def chain(self):
-        return self._chain
-
-    @property
-    def amino_acid(self) -> AminoAcid:
-        return self._amino_acid
-
-    @property
-    def atoms(self) -> List[Atom]:
-        return self._atoms
-
-    @property
-    def number_string(self) -> str:
-        "contains both the number and the insertion code (if any)"
-
-        if self._insertion_code is not None:
-            return "{}{}".format(self._number, self._insertion_code)
-        else:
-            return str(self._number)
-
-    @property
-    def insertion_code(self) -> str:
-        return self._insertion_code
-
-    def add_atom(self, atom: Atom):
-        self._atoms.append(atom)
-
-    def __repr__(self) -> str:
-        return "{} {}".format(self._chain, self.number_string)
-
-
-class Chain:
-    "represents one pdb chain"
-
-    def __init__(self, model, id_: str):
-        """
-        Args:
-            model(deeprank structure object): the model that this chain is part of
-            id_(str): the pdb identifier of this chain
-        """
-
-        self._model = model
-        self._id = id_
-        self._residues = []
-        self._pssm = None  # pssm is per chain
-
-    @property
-    def model(self):
-        return self._model
-
-    @property
-    def pssm(self) -> ConservationRow:
-        return self._pssm
-
-    @pssm.setter
-    def pssm(self, pssm: ConservationRow):
-        self._pssm = pssm
-
-    def add_residue(self, residue: Residue):
-        self._residues.append(residue)
-
-    @property
-    def id(self) -> str:
-        return self._id
-
-    @property
-    def residues(self) -> List[Residue]:
-        return self._residues
-
-    def __eq__(self, other) -> bool:
-        return type(self) == type(other) and self._model == other._model and self._id == other._id
-
-    def __hash__(self) -> hash:
-        return hash((self._model, self._id))
-
-    def __repr__(self) -> str:
-        return "{} {}".format(self._model, self._id)
-
-
-class Structure:
-    "represents one entire pdb structure"
-
-    def __init__(self, id_: str = None):
-        """
-            Args:
-                id_(str): an unique identifier for this structure, can be the pdb accession code.
-        """
-        self._id = id_
-        self._chains = {}
-
-    def __eq__(self, other) -> bool:
-        return type(self) == type(other) and self._id == other._id
-
-    def __hash__(self) -> hash:
-        return hash(self._id)
-
-    def __repr__(self) -> str:
-        return self._id
-
-    def get_chain(self, chain_id: str) -> Chain:
-        return self._chains[chain_id]
-
-    def add_chain(self, chain: Chain):
-        if chain.id in self._chains:
-            raise ValueError("duplicate chain: {}".format(chain.id))
-
-        self._chains[chain.id] = chain
-
-    @property
-    def chains(self) -> List[Chain]:
-        return list(self._chains.values())
-
-    def get_atoms(self) -> List[Atom]:
-        "shortcut to list all atoms in this structure"
-        atoms = []
-        for chain in self._chains.values():
-            for residue in chain.residues:
-                atoms.extend(residue.atoms)
-
-        return atoms
