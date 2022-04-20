0d31086b3c14c2fa45685a722c27e97fdc7b4d5e
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2022-04-20 13:25:25 +0200

    Use preprocessed train data set for neural network tests

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -1,15 +1,12 @@
-import tempfile
 import shutil
-import os
-
+import tempfile
 import unittest
 
-from deeprank_gnn.tools.CustomizeGraph import add_target
-from deeprank_gnn.GraphGenMP import GraphHDF5
 from deeprank_gnn.NeuralNet import NeuralNet
-from deeprank_gnn.ginet import GINet
 from deeprank_gnn.foutnet import FoutNet
+from deeprank_gnn.ginet import GINet
 from deeprank_gnn.sGAT import sGAT
+from tests.utils import PATH_TEST
 
 
 def _model_base_test(work_directory, hdf5_path, model, task='reg', target='irmsd', plot=False):
@@ -42,31 +39,10 @@ def _model_base_test(work_directory, hdf5_path, model, task='reg', target='irmsd
 class TestNeuralNet(unittest.TestCase):
 
     def setUp(self):
-        f, self.hdf5_path = tempfile.mkstemp(prefix="1ATN_residue", suffix=".hdf5")
-        os.close(f)
-
+        self.hdf5_path = str(PATH_TEST / 'data/train_ref/1ATN_train_data.hdf5')
         self.work_directory = tempfile.mkdtemp()
 
-        GraphHDF5(pdb_path='./tests/data/pdb/1ATN/',
-                  ref_path='./tests/data/ref/1ATN/',
-                  pssm_path='./tests/data/pssm/1ATN/',
-                  graph_type='residue', outfile=self.hdf5_path,
-                  nproc=1, tmpdir=self.work_directory, biopython=True)
-
-        f, target_path = tempfile.mkstemp()
-        os.close(f)
-
-        try:
-            with open(target_path, 'w') as f:
-                for i in range(1, 5):
-                    f.write('residue-ppi-1ATN_%dw:A-B %d\n' % (i, i % 2 == 0))
-
-            add_target(self.hdf5_path, "bin_class", target_path)
-        finally:
-            os.remove(target_path)
-
     def tearDown(self):
-        os.remove(self.hdf5_path)
         shutil.rmtree(self.work_directory)
 
     def test_ginet(self):
