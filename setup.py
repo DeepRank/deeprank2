1dbb1512fbfa4ed90ce1de109f68dfa93ffe23fa
Coos Baakman <cbaakman@radboudumc.nl>
2022-05-30 11:42:09 +0200

    add the dependencies

--- setup.py
+++ setup.py
@@ -48,7 +48,8 @@ setup(
         'chart-studio >= 1.1.0', 'BioPython >= 1.79', 'python-louvain >= 0.16',
         'markov-clustering >= 0.0.6.dev0',
         'tqdm >= 4.63.0', 'freesasa >= 2.1.0',
-        'tensorboard >= 2.9.0', 'torchvision >= 0.12.0'
+        'tensorboard >= 2.9.0', 'torchvision >= 0.12.0',
+        'importlib >= 1.0.4', 'functools >= 0.5'
     ],
     extras_require={
         'dev': ['yapf', 'isort'],
