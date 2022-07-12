1d4c70e2783efb41d2a3ed8e76fffb8421078753
gcroci2 <crocioni.giulia@gmail.com>
2022-07-12 10:25:30 +0200

    change prints to log

--- deeprankcore/preprocess.py
+++ deeprankcore/preprocess.py
@@ -44,17 +44,17 @@ def preprocess(feature_modules: List, queries: List[Query],
         # which limits which CPUs a process and its children can run on.
         process_count = len(os.sched_getaffinity(0))
 
-    print(f'\nSet of CPU processors available: {process_count}.')
+    _log.info(f'\nSet of CPU processors available: {process_count}.')
 
     if prefix is None:
         prefix = "preprocessed-data"
 
-    print('Creating pool function to process the queries...')
+    _log.info('Creating pool function to process the queries...')
     pool_function = partial(_preprocess_one_query, prefix,
                             [m.__name__ for m in feature_modules])
 
     with Pool(process_count) as pool:
-        print('Starting pooling...\n')
+        _log.info('Starting pooling...\n')
         pool.map(pool_function, queries)
 
     output_paths = glob(f"{prefix}-*.hdf5")
