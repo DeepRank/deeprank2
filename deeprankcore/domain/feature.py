be7b53a0f77ba5883e72fec22e37b2846be29339
DaniBodor <d.bodor@esciencecenter.nl>
2022-09-29 11:28:41 +0200

    renamed potentials and added temporary to do list

--- deeprankcore/domain/feature.py
+++ deeprankcore/domain/feature.py
@@ -39,8 +39,8 @@ FEATURE_NODE_DIFFERENCECONSERVATION = "difference_conservation" # FEATURENAME_PS
 
 # edge features
 FEATURE_EDGE_COVALENT = "covalent" # FEATURENAME_COVALENT = "covalent" : bool
-FEATURE_EDGE_ELECTROSTATICPOTENTIAL = "electrostatic_potential" # FEATURENAME_EDGECOULOMB = "coulomb"
-FEATURE_EDGE_LJPOTENTIAL = "lj_potential" # FEATURENAME_EDGEVANDERWAALS = "vanderwaals"
+FEATURE_EDGE_ELECTROSTATIC = "electrostatic" # FEATURENAME_EDGECOULOMB = "coulomb"
+FEATURE_EDGE_VANDERWAALS = "vanderwaals" # FEATURENAME_EDGEVANDERWAALS = "vanderwaals"
 FEATURE_EDGE_DISTANCE = "distance" # FEATURENAME_EDGEDISTANCE = "dist"
 # FEATURE_EDGE_INTERACTIONTYPE = "interaction_type" # FEATURENAME_EDGETYPE = "type" --> replace by CIS/SAMECHAIN
 FEATURE_EDGE_CIS = "cis" # FEATURENAME_EDGESAMECHAIN = "same_chain" # bool --> unused; I think superceded by INTERACTIONTYPE, but this parameter makes more sense to me
--- deeprankcore/domain/storage.py
+++ deeprankcore/domain/storage.py
@@ -15,3 +15,16 @@ HDF5KEY_GRID_Z = "z"
 HDF5KEY_GRID_CENTER = "center"
 HDF5KEY_GRID_MAPPEDFEATURES = "mapped_features"
 HDF5KEY_GRID_MAPPEDFEATURESVALUE = "mapped_features_value"
+
+
+
+# TO DO LIST:
+# - Search/replace outdated nomenclature inside strings: node_data, edge_data, score, edges, nodes, edge_index
+#   - is there a way to fuse strings with a given character in between????
+# - Update FEATURENAMEs
+#   - make sure to search for strings as well
+# - Fix clustering groups/names
+# - Move above stuff to deeprankcore.domain.feature instead?
+# - Is grid stuff relevant here (or is it a CNN thing)?
+# - Check featurename changes suggested by Giulia
+#   - Features that Giulia suggested to exclude; how about assigning them instead?
