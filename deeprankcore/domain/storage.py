daec48804e4430ff6a6c0597a910f6c3ce29c023
DaniBodor <d.bodor@esciencecenter.nl>
2022-10-03 16:06:56 +0200

    deleted to do list

--- deeprankcore/domain/storage.py
+++ deeprankcore/domain/storage.py
@@ -16,16 +16,3 @@ HDF5KEY_GRID_CENTER = "center"
 HDF5KEY_GRID_MAPPEDFEATURES = "mapped_features"
 HDF5KEY_GRID_MAPPEDFEATURESVALUE = "mapped_features_value"
 
-
-
-# TO DO LIST:
-# - Search/replace outdated nomenclature inside strings: node_data, edge_data, score, edges, nodes, edge_index
-#   - is there a way to fuse strings with a given character in between????
-# - Update FEATURENAMEs (done until res_type)
-#   - make sure to search for strings as well
-#   - move featurenames into dict instead of individual strings
-# - Fix clustering groups/names
-# - Move above stuff to deeprankcore.domain.feature instead?
-# - Is grid stuff relevant here (or is it a CNN thing)?
-# - Check featurename changes suggested by Giulia
-#   - Features that Giulia suggested to exclude; how about assigning them instead?
