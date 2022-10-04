1a1716c88af1e28c24e9febd9110f86240298fc8
DaniBodor <d.bodor@esciencecenter.nl>
2022-10-04 14:07:48 +0200

    add separate feature submodules

---
+++ deeprankcore/domain/features/edge.py
@@ -0,0 +1,10 @@
+FEATURE_EDGE_COVALENT = "covalent" # FEATURENAME_COVALENT = "covalent" : bool
+FEATURE_EDGE_ELECTROSTATIC = "electrostatic" # FEATURENAME_EDGECOULOMB = "coulomb"
+FEATURE_EDGE_VANDERWAALS = "vanderwaals" # FEATURENAME_EDGEVANDERWAALS = "vanderwaals"
+FEATURE_EDGE_DISTANCE = "distance" # FEATURENAME_EDGEDISTANCE = "dist"
+# FEATURE_EDGE_INTERACTIONTYPE = "interaction_type" # FEATURENAME_EDGETYPE = "type" --> replace by CIS/SAMECHAIN
+FEATURE_EDGE_CIS = "cis" # FEATURENAME_EDGESAMECHAIN = "same_chain" # bool --> unused; I think superceded by INTERACTIONTYPE, but this parameter makes more sense to me
+
+# ## edge types --> use EDGE_CIS instead?
+# FEATURE_EDGE_CISINTERACTION = "cis_interaction" # EDGETYPE_INTERNAL = "internal"
+# FEATURE_EDGE_TRANSINTERACTION = "trans_interaction"# EDGETYPE_INTERFACE = "interface"
