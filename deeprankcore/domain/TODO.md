751ac6396484b3a9d00100b37b7b79389a14f6a7
DaniBodor <d.bodor@esciencecenter.nl>
2022-10-05 21:46:28 +0200

    reorganized nodefeats and updated TO DO

--- deeprankcore/domain/TODO.md
+++ deeprankcore/domain/TODO.md
@@ -1,8 +1,7 @@
 # My to do list for this issue and #145:
 
-## [X] Update FEATURENAMEs (done until res_type)
+## [ ] Update FEATURENAMEs (done until res_type)
     - [X] make sure to search for strings as well
-    - [X] move featurenames into dict instead of individual strings??
     - [X] move all this into submodules
     - [X] Double check all the tests for node/edge features
     - [X] Double check README, quickstart, and examples
@@ -11,6 +10,7 @@
     - [X] Move domain.storage
         - [x] delete domain/storage.py
     - [x] delete domain/feature.py
+    - [ ] shorten/clarify some of the feature names?
 
 ## [ ] Update clustering groups/names
     - [X] Update cluster naming to one level shallower and prefixing the clustering-type
@@ -33,7 +33,7 @@
             - Don't know how to get this info
                 - models/structure has a Chain class
         - [ ] SAMECHAIN always set to 1
-        - [ ] check whether all other features are actually assigned
+        - [X] check whether all other features are actually assigned
             - tools.visualization, line 83: `graph.edges[node1_name, node2_name][edgefeats.SAMECHAIN] = 1.0` without any if statement
             - currently there is no chain info anyway, but once we have that, this would be useful
             - would need to find out how to extract CHAINID from the two nodes of this edge
@@ -44,6 +44,6 @@
 
 ## [ ] Rethink definitions of deeprankcore/domain vs deeprankcore/models
     - [X] deeprankcore/domain/amino-acid --> add to deeprankcore/models/amino-acid
-    - [ ] deeprankcore/models/polarity --> add to deeprankcore/models/amino-acid
+    - [X] deeprankcore/models/polarity --> add to deeprankcore/models/amino-acid
     - [ ] deeprankcore/models/error --> move to deeprankcore/domain/errors (and start filling it up)
     - [ ] potentially merge the two folders
--- deeprankcore/domain/features/nodefeats.py
+++ deeprankcore/domain/features/nodefeats.py
@@ -5,25 +5,25 @@ POSITION = "position" # list[3xfloat]; former FEATURENAME_POSITION
 ## residue core features
 RESTYPE = "res_type" # AminoAcid object; former FEATURENAME_AMINOACID
 RESCHARGE = "res_charge" # float(<0); former FEATURE_NODE_RESCHARGE
-POLARITY = "polarity" #  Polarity object; former FEATURE_NODE_POLARITY
 RESSIZE = "res_size" # int; former FEATURENAME_SIZE
+POLARITY = "polarity" #  Polarity object; former FEATURE_NODE_POLARITY
 HBDONORS = "hb_donors" # int; former FEATURENAME_HYDROGENBONDDONORS
 HBACCEPTORS = "hb_acceptors"# int; former FEATURENAME_HYDROGENBONDACCEPTORS
-
-## variant features
-VARIANTRES = "variant_res" # AminoAcid object; former FEATURENAME_VARIANTAMINOACID
-VARIANTCONSERVATION = "variant_conservation" # int; former FEATURENAME_PSSMVARIANT
-DIFFCONSERVATION = "diff_conservation" # int; former FEATURENAME_PSSMDIFFERENCE
-DIFFSIZE = "diff_size" # int; former FEATURENAME_SIZEDIFFERENCE
-DIFFPOLARITY = "diff_polarity" # [type?]; former FEATURENAME_POLARITYDIFFERENCE
-DIFFHBDONORS = "diff_hb_donors" # int; former FEATURENAME_HYDROGENBONDDONORSDIFFERENCE
-DIFFHBACCEPTORS = "diff_hb_acceptors" # int; former FEATURENAME_HYDROGENBONDACCEPTORSDIFFERENCE
-DIFFCHARGE = "diff_charge" # if we include CHARGE, it probably makes sense to include this one too
 
 ## conservation features
 PSSM = "pssm" # list[20xint]; former FEATURENAME_PSSM
 INFOCONTENT = "info_content" # float; former FEATURENAME_INFORMATIONCONTENT
 CONSERVATION = "conservation" # int; former FEATURENAME_PSSMWILDTYPE
+
+## variant features
+VARIANTRES = "variant_res" # AminoAcid object; former FEATURENAME_VARIANTAMINOACID
+DIFFCHARGE = "diff_charge" # if we include CHARGE, it probably makes sense to include this one too
+DIFFSIZE = "diff_size" # int; former FEATURENAME_SIZEDIFFERENCE
+DIFFPOLARITY = "diff_polarity" # [type?]; former FEATURENAME_POLARITYDIFFERENCE
+DIFFHBDONORS = "diff_hb_donors" # int; former FEATURENAME_HYDROGENBONDDONORSDIFFERENCE
+DIFFHBACCEPTORS = "diff_hb_acceptors" # int; former FEATURENAME_HYDROGENBONDACCEPTORSDIFFERENCE
+DIFFCONSERVATION = "diff_conservation" # int; former FEATURENAME_PSSMDIFFERENCE
+VARIANTCONSERVATION = "variant_conservation" # int; former FEATURENAME_PSSMVARIANT
 
 ## protein context features
 BSA = "bsa" # float; former FEATURENAME_BURIEDSURFACEAREA
