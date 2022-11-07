b921bfd0ad47339ff51f2df63f38d9d3cdbee62b
DaniBodor <d.bodor@esciencecenter.nl>
2022-11-07 18:04:45 +0100

    add_features with one-shot calculations

--- deeprankcore/feature/atomic_contact.py
+++ deeprankcore/feature/atomic_contact.py
@@ -1,5 +1,6 @@
 from typing import List
 import logging
+import warnings
 import numpy as np
 from scipy.spatial import distance_matrix
 from deeprankcore.models.structure import Atom
@@ -52,31 +53,67 @@ def _get_lennard_jones_potentials(atoms: List[Atom], distances: np.ndarray) -> n
 
 
 def add_features(pdb_path: str, graph: Graph, *args, **kwargs): # pylint: disable=unused-argument
-    for edge in graph.edges:
-        contact = edge.id
+    # get a set of all the atoms involved with a unique index
+    ## create an empty set
+    all_atoms = set() 
+    ## add all atoms of all edges to the set
+    if isinstance(graph.edges[0].id, AtomicContact):
+        for edge in graph.edges:
+            contact = edge.id
+            all_atoms.add(contact.atom1)
+            all_atoms.add(contact.atom2)
+    elif isinstance(graph.edges[0].id, ResidueContact):
+        for edge in graph.edges:
+            contact = edge.id
+            for atom in (contact.residue1.atoms + contact.residue2.atoms):
+                all_atoms.add(atom)
+    ## convert the set to a list
+    all_atoms = list(all_atoms)
 
-        if isinstance(contact, ResidueContact):
-            atoms1 = contact.residue1.atoms
-            atoms2 = contact.residue2.atoms
-        elif isinstance(contact, AtomicContact):
-            atoms1 = [contact.atom1]
-            atoms2 = [contact.atom2]
+
+    # make calculations once per graph
+    ## calculate the pairwise distances between all atoms
+    positions = [atom.position for atom in all_atoms]
+    interatomic_distances = distance_matrix(positions, positions)
+    ## calculate the pairwise potentials between all atoms
+    with warnings.catch_warnings(record=RuntimeWarning):
+        warnings.simplefilter("ignore")
+        interatomic_electrostatic_potentials = _get_coulomb_potentials(all_atoms, all_atoms, interatomic_distances)
+        interatomic_vanderwaals_potentials = _get_lennard_jones_potentials(all_atoms, interatomic_distances)
+
+    # generate dictionary with an index for each unique atom
+    all_atoms = {all_atoms[i]: i for i in range(len(all_atoms))}
+
+    if isinstance(graph.edges[0].id, AtomicContact):
+        for edge in graph.edges:        
+            ## find the indices
+            contact = edge.id
+            atom1_index = all_atoms[contact.atom1]
+            atom2_index = all_atoms[contact.atom2]
+            ## set features
             edge.features[Efeat.SAMERES] = float( contact.atom1.residue == contact.atom2.residue) # 1.0 for True; 0.0 for False
-        else:
-            raise TypeError(
-                f"Unexpected edge type: {type(contact)} for {edge}")
+            edge.features[Efeat.SAMECHAIN] = float( contact.atom1.residue.chain == contact.atom1.residue.chain ) # 1.0 for True; 0.0 for False
+            edge.features[Efeat.DISTANCE] = interatomic_distances[atom1_index, atom2_index]
+            edge.features[Efeat.COVALENT] = float( edge.features[Efeat.DISTANCE] < MAX_COVALENT_DISTANCE ) # 1.0 for True; 0.0 for False
+            edge.features[Efeat.ELECTROSTATIC] = interatomic_electrostatic_potentials[atom1_index, atom2_index]
+            if edge.features[Efeat.SAMERES]:
+                edge.features[Efeat.VANDERWAALS] = interatomic_vanderwaals_potentials['intra'][atom1_index, atom2_index]
+            else:
+                edge.features[Efeat.VANDERWAALS] = interatomic_vanderwaals_potentials['inter'][atom1_index, atom2_index]
+    
+    elif isinstance(contact, ResidueContact):
+        for edge in graph.edges:        
+            ## find the indices
+            contact = edge.id
+            atom1_indices = [all_atoms[atom] for atom in contact.residue1.atoms]
+            atom2_indices = [all_atoms[atom] for atom in contact.residue2.atoms]
+            ## set features
+            edge.features[Efeat.SAMECHAIN] = float( contact.residue1.chain == contact.residue2.chain ) # 1.0 for True; 0.0 for False
+            edge.features[Efeat.DISTANCE] = np.min([[interatomic_distances[a1, a2] for a1 in atom1_indices] for a2 in atom2_indices])
+            edge.features[Efeat.COVALENT] = float( edge.features[Efeat.DISTANCE] < MAX_COVALENT_DISTANCE ) # 1.0 for True; 0.0 for False
+            edge.features[Efeat.ELECTROSTATIC] = np.sum([[interatomic_electrostatic_potentials[a1, a2] for a1 in atom1_indices] for a2 in atom2_indices])
+            edge.features[Efeat.VANDERWAALS] = np.sum([[interatomic_vanderwaals_potentials['inter'][a1, a2] for a1 in atom1_indices] for a2 in atom2_indices])
 
-        # calculate the distances matrix
-        interatomic_distances = distance_matrix([atom.position for atom in atoms1], 
-                                                [atom.position for atom in atoms2])
-
-        # calculate potentials matrices
-        interatomic_electrostatic_potentials = _get_coulomb_potentials(atoms1, atoms2, interatomic_distances)
-        interatomic_vanderwaals_potentials = _get_lennard_jones_potentials(atoms1, atoms2, interatomic_distances)
-
-        # set features
-        edge.features[Efeat.DISTANCE] = np.min(interatomic_distances) # minimum atom distance is considered as the distance between 2 residues
-        edge.features[Efeat.ELECTROSTATIC] = np.sum(interatomic_electrostatic_potentials)
-        edge.features[Efeat.VANDERWAALS] = np.sum(interatomic_vanderwaals_potentials)
-        edge.features[Efeat.SAMECHAIN] = float( atoms1[0].residue.chain == atoms2[0].residue.chain ) # 1.0 for True; 0.0 for False
-        edge.features[Efeat.COVALENT] = float( edge.features[Efeat.DISTANCE] < MAX_COVALENT_DISTANCE ) # 1.0 for True; 0.0 for False
+    else:
+        raise TypeError(
+            f"Unexpected edge type: {type(contact)} for {edge}")
