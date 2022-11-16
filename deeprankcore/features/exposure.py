94e570bbf5354250545fb78f612c4f091d8c90f4
DaniBodor <d.bodor@esciencecenter.nl>
2022-11-16 05:19:56 +0100

    suppress PDBConstructionWarning

--- deeprankcore/features/exposure.py
+++ deeprankcore/features/exposure.py
@@ -1,5 +1,7 @@
 import logging
 import numpy as np
+import warnings
+from Bio.PDB.Atom import PDBConstructionWarning
 from Bio.PDB.PDBParser import PDBParser
 from Bio.PDB.ResidueDepth import get_surface, residue_depth
 from Bio.PDB.HSExposure import HSExposureCA
@@ -21,26 +23,27 @@ def space_if_none(value):
 
 def add_features(pdb_path: str, graph: Graph, *args, **kwargs): # pylint: disable=unused-argument
 
-    parser = PDBParser()
-    structure = parser.get_structure('_tmp', pdb_path)
+    with warnings.catch_warnings(record=PDBConstructionWarning):
+        warnings.simplefilter("ignore")
+        parser = PDBParser()
+        structure = parser.get_structure('_tmp', pdb_path)
+    
     bio_model = structure[0]
-
     surface = get_surface(bio_model)
     hse = HSExposureCA(bio_model)
 
     for node in graph.nodes:
 
+        # These can only be calculated per residue, not per atom.
+        # So for atomic graphs, every atom gets its residue's value.
         if isinstance(node.id, Atom):
             atom = node.id
             residue = atom.residue
-
         elif isinstance(node.id, Residue):
             residue = node.id
         else:
             raise TypeError(f"Unexpected node type: {type(node)}")
 
-        # These can only be calculated per residue, not per atom.
-        # So for atomic graphs, every atom gets its residue's value.
         bio_residue = bio_model[residue.chain.id][residue.number]
         node.features[Nfeat.RESDEPTH] = residue_depth(bio_residue, surface)
 
