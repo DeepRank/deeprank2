2f96b3245c7a7eb3663fcfa0fd49007b7a0c04e4
DaniBodor <d.bodor@esciencecenter.nl>
2022-11-23 13:53:34 +0100

    update test_dataset

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,12 +1,9 @@
 import unittest
 from deeprankcore.dataset import GraphDataset, save_hdf5_keys
-from deeprankcore.trainer import _DivideDataSet
 from torch_geometric.data.data import Data
 import h5py
-from deeprankcore.domain import (edgestorage as Efeat, nodestorage as Nfeat,
-                                targetstorage as targets)
+from deeprankcore.domain import targetstorage as targets
 
-node_feats = [Nfeat.RESTYPE, Nfeat.POLARITY, Nfeat.BSA, Nfeat.RESDEPTH, Nfeat.HSE, Nfeat.INFOCONTENT, Nfeat.PSSM]
 
 class TestDataSet(unittest.TestCase):
     def setUp(self):
@@ -15,19 +12,13 @@ class TestDataSet(unittest.TestCase):
     def test_dataset(self):
         GraphDataset(
             hdf5_path=self.hdf5_path,
-            node_feature=node_feats,
-            edge_feature=[Efeat.DISTANCE],
             target=targets.IRMSD,
-            subset=None,
         )
 
     def test_dataset_filter(self):
         GraphDataset(
             hdf5_path=self.hdf5_path,
-            node_feature=node_feats,
-            edge_feature=[Efeat.DISTANCE],
             target=targets.IRMSD,
-            subset=None,
             dict_filter={targets.IRMSD: "<10"},
         )
 
@@ -39,10 +30,7 @@ class TestDataSet(unittest.TestCase):
 
         dataset = GraphDataset(
             hdf5_path=self.hdf5_path,
-            node_feature=node_feats,
-            edge_feature=[Efeat.DISTANCE],
             target=targets.IRMSD,
-            subset=None,
             transform=operator
         )
 
@@ -52,8 +40,6 @@ class TestDataSet(unittest.TestCase):
     def test_multi_file_dataset(self):
         dataset = GraphDataset(
             hdf5_path=["tests/data/hdf5/train.hdf5", "tests/data/hdf5/valid.hdf5"],
-            node_feature=node_feats,
-            edge_feature=[Efeat.DISTANCE],
             target=targets.BINARY
         )
 
@@ -91,45 +77,6 @@ class TestDataSet(unittest.TestCase):
         assert len(new_ids) == n
         for new_id in new_ids:
             assert new_id in original_ids
-
-    def test_trainsize(self):
-        hdf5 = "tests/data/hdf5/train.hdf5"
-        hdf5_file = h5py.File(hdf5, 'r')    # contains 44 datapoints
-        n_val = int ( 0.25 * len(hdf5_file) )
-        n_train = len(hdf5_file) - n_val
-        test_cases = [None, 0.25, n_val]
-        
-        for t in test_cases:
-            dataset_train, dataset_val =_DivideDataSet(
-                dataset = GraphDataset(hdf5_path=hdf5),
-                val_size=t,
-            )
-
-            assert len(dataset_train) == n_train
-            assert len(dataset_val) == n_val
-
-        hdf5_file.close()
-        
-    def test_invalid_trainsize(self):
-
-        hdf5 = "tests/data/hdf5/train.hdf5"
-        hdf5_file = h5py.File(hdf5, 'r')    # contains 44 datapoints
-        n = len(hdf5_file)
-        test_cases = [
-            1.0, n,     # cannot be 100% validation data
-            -0.5, -1,   # no negative values 
-            1.1, n + 1, # cannot use more than all data as input
-            ]
-        
-        for t in test_cases:
-            print(t)
-            with self.assertRaises(ValueError):
-                _DivideDataSet(
-                    dataset = GraphDataset(hdf5_path=hdf5),
-                    val_size=t,
-                )
-        
-        hdf5_file.close()
 
     def test_subset(self):
         hdf5 = h5py.File("tests/data/hdf5/train.hdf5", 'r')  # contains 44 datapoints
--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -6,6 +6,7 @@ import pytest
 import logging
 import warnings
 import torch
+import h5py
 from deeprankcore.trainer import Trainer
 from deeprankcore.dataset import GraphDataset
 from deeprankcore.neuralnets.ginet import GINet
@@ -19,6 +20,7 @@ from deeprankcore.utils.metrics import (
 )
 from deeprankcore.domain import (edgestorage as Efeat, nodestorage as Nfeat,
                                 targetstorage as targets)
+from deeprankcore.trainer import _divide_dataset
 
 
 _log = logging.getLogger(__name__)
@@ -419,6 +421,44 @@ class TestTrainer(unittest.TestCase):
         else:
             warnings.warn("CUDA NOT AVAILABLE. test_cuda skipped")
             _log.debug("cuda is not available, test_cuda skipped")
+
+    def test_trainsize(self):
+        hdf5 = "tests/data/hdf5/train.hdf5"
+        hdf5_file = h5py.File(hdf5, 'r')    # contains 44 datapoints
+        n_val = int ( 0.25 * len(hdf5_file) )
+        n_train = len(hdf5_file) - n_val
+        test_cases = [None, 0.25, n_val]
+        
+        for t in test_cases:
+            dataset_train, dataset_val =_divide_dataset(
+                dataset = GraphDataset(hdf5_path=hdf5),
+                val_size=t,
+            )
+
+            assert len(dataset_train) == n_train
+            assert len(dataset_val) == n_val
+
+        hdf5_file.close()
+        
+    def test_invalid_trainsize(self):
+        hdf5 = "tests/data/hdf5/train.hdf5"
+        hdf5_file = h5py.File(hdf5, 'r')    # contains 44 datapoints
+        n = len(hdf5_file)
+        test_cases = [
+            1.0, n,     # cannot be 100% validation data
+            -0.5, -1,   # no negative values 
+            1.1, n + 1, # cannot use more than all data as input
+            ]
+        
+        for t in test_cases:
+            print(t)
+            with self.assertRaises(ValueError):
+                _divide_dataset(
+                    dataset = GraphDataset(hdf5_path=hdf5),
+                    val_size=t,
+                )
+        
+        hdf5_file.close()
 
 
 if __name__ == "__main__":
