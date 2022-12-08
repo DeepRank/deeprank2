1a4dfec0063d7880d91bc27f5109ee1b29fb185d
DaniBodor <d.bodor@esciencecenter.nl>
2022-12-08 20:36:41 +0100

    fix test_aminoacidlist

--- tests/domain/test_aminoacidlist.py
+++ tests/domain/test_aminoacidlist.py
@@ -1,12 +1,21 @@
 import numpy as np
 from deeprankcore.domain.aminoacidlist import amino_acids
+from deeprankcore.domain.aminoacidlist import cysteine, selenocysteine, lysine, pyrrolysine
 
+EXCEPTIONS = [
+    [cysteine, selenocysteine],
+    [lysine, pyrrolysine],
+]
 
 def test_all_different_onehot():
-
     for amino_acid in amino_acids:
         for other in amino_acids:
             if other != amino_acid:
-                assert not np.all(
-                    amino_acid.onehot == other.onehot
-                ), f"{amino_acid.onehot} is occupied by both {other} and {amino_acid}"
+                if other in EXCEPTIONS[0] and amino_acid in EXCEPTIONS[0]:
+                    assert np.all(amino_acid.onehot == other.onehot)
+                elif other in EXCEPTIONS[1] and amino_acid in EXCEPTIONS[1]:
+                    assert np.all(amino_acid.onehot == other.onehot)
+                else:
+                    assert not np.all(
+                        amino_acid.onehot == other.onehot
+                    ), f"one-hot index {amino_acid.index} is occupied by both {amino_acid} and {other}"
