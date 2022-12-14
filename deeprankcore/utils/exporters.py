bf1b3d9c3e4f3de7cd740946285070eb08558ed6
gcroci2 <crocioni.giulia@gmail.com>
2022-12-14 16:30:59 +0100

    add reset index to pandas df in exporters

--- deeprankcore/utils/exporters.py
+++ deeprankcore/utils/exporters.py
@@ -282,3 +282,4 @@ class HDF5OutputExporter(OutputExporter):
         df_epoch = pd.DataFrame(data=d_epoch)
 
         self.df = pd.concat([self.df, df_epoch])
+        self.df.reset_index(drop=True, inplace=True)
