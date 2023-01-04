ef57d57ec4c7ea0f790845e9f5041ac8de3aa3ce
gcroci2 <crocioni.giulia@gmail.com>
2023-01-04 19:39:46 +0100

    update tests

--- tests/tools/test_transform.py
+++ tests/tools/test_transform.py
@@ -1,6 +1,8 @@
+from tempfile import mkdtemp
+from shutil import rmtree
+import os
 import h5py
-from deeprankcore.tools.transform import hdf5_to_pandas, plot_hist
-import plotly
+from deeprankcore.tools.transform import hdf5_to_pandas, save_hist
 
 def test_hdf5_to_pandas():
 
@@ -25,11 +27,17 @@ def test_hdf5_to_pandas():
 
     assert df.shape[0] == len(keys[2:])
     
-def test_plot_hist():
+def test_save_hist():
 
+    output_directory = mkdtemp()
+    fname = os.path.join(output_directory, "test.png")
     hdf5_path = "tests/data/hdf5/test.hdf5"
+
     df = hdf5_to_pandas(
         hdf5_path)
-    fig = plot_hist(df, ['charge', 'binary'])
 
-    assert isinstance(fig, plotly.graph_objs._figure.Figure)
+    save_hist(df, ['charge', 'binary'], fname = fname)
+
+    assert len(os.listdir(output_directory)) > 0
+
+    rmtree(output_directory)
