266984e62c1e07f9c69caeaa1ce604b16e6bbf70
Coos Baakman <cbaakman@radboudumc.nl>
2023-01-09 12:14:59 +0100

    check grid orientation

--- tests/utils/test_grid.py
+++ tests/utils/test_grid.py
@@ -2,10 +2,11 @@ from tempfile import mkstemp
 import os
 
 import h5py
+import numpy as np
 
 from deeprankcore.query import ProteinProteinInterfaceAtomicQuery
 import deeprankcore.features.contact
-from deeprankcore.utils.grid import MapMethod, Grid, GridSettings
+from deeprankcore.utils.grid import MapMethod, GridSettings
 
 
 def test_grid_orientation():
@@ -17,13 +18,40 @@ def test_grid_orientation():
         target_ys = grid_points_group["y"][()]
         target_zs = grid_points_group["z"][()]
 
-    query = ProteinProteinInterfaceAtomicQuery("tests/data/pdb/1ak5/1ak5.pdb",
+    query = ProteinProteinInterfaceAtomicQuery("tests/data/pdb/1ak4/1ak4.pdb",
                                                "C", "D",
                                                distance_cutoff=8.5)
 
-    graph = query.build([contact])
+    graph = query.build([deeprankcore.features.contact])
 
-    grid_path, grid_file = mkstemp(suffix=".hdf5")
+    grid_file, grid_path = mkstemp(suffix=".hdf5")
     os.close(grid_file)
 
-    graph.write_as_grid_to_hdf5
+    points_counts = [target_xs.shape[0], target_ys.shape[0], target_zs.shape[0]]
+    grid_sizes = [np.max(target_xs) - np.min(target_xs),
+                  np.max(target_ys) - np.min(target_ys),
+                  np.max(target_zs) - np.min(target_zs)]
+
+    grid_settings = GridSettings(points_counts, grid_sizes)
+
+    graph.write_as_grid_to_hdf5(grid_path, grid_settings, MapMethod.GAUSSIAN)
+
+    try:
+        with h5py.File(grid_path, 'r') as data_file:
+            entry_group = data_file[list(data_file.keys())[0]]
+            grid_points_group = entry_group["grid_points"]
+
+            xs = grid_points_group["x"][()]
+            ys = grid_points_group["y"][()]
+            zs = grid_points_group["z"][()]
+    finally:
+        os.remove(grid_path)
+
+    assert xs.shape == target_xs.shape
+    assert np.all(xs == target_xs)
+
+    assert ys.shape == target_ys.shape
+    assert np.all(ys == target_ys)
+
+    assert zs.shape == target_zs.shape
+    assert np.all(zs == target_zs)
