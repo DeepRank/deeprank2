fix the rotation seed

tests/utils/test_graph.py
@@ -69,10 +69,10 @@ def test_graph_build_and_export(): # pylint: disable=too-many-locals
         graph.write_to_hdf5(hdf5_path)
 
         # export grid to hdf5
-        grid_settings = GridSettings([20, 21, 21], [20.0, 21.0, 21.0])
+        grid_settings = GridSettings([20, 20, 20], [20.0, 20.0, 20.0])
         assert np.all(grid_settings.resolutions == np.array((1.0, 1.0, 1.0)))
 
-        axis, angle = get_rot_axis_angle(int(time() * 1000) % (2**32 - 1))
+        axis, angle = get_rot_axis_angle(412346587)
         augmentation = Augmentation(axis, angle)
 
         graph.write_as_grid_to_hdf5(hdf5_path, grid_settings, MapMethod.GAUSSIAN)