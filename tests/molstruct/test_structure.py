d89b9117c0c766e994855eafc9104c9babec563c
Coos Baakman <cbaakman@radboudumc.nl>
2023-02-03 11:45:28 +0100

    hash dependency fix

--- tests/molstruct/test_structure.py
+++ tests/molstruct/test_structure.py
@@ -1,20 +1,47 @@
 
+from multiprocessing.connection import _ForkingPickler
 import pickle
 from pdb2sql import pdb2sql
 
 from deeprankcore.utils.buildgraph import get_structure
+from deeprankcore.molstruct.structure import PDBStructure
 
 
-def test_serialization():
-
-    path = "tests/data/pdb/101M/101M.pdb"
+def _get_structure(path) -> PDBStructure:
     pdb = pdb2sql(path)
     try:
         structure = get_structure(pdb, "101M")
     finally:
         pdb._close()
 
+    assert structure is not None
+
+    return structure
+
+
+def test_serialization_pickle():
+
+    structure = _get_structure("tests/data/pdb/101M/101M.pdb")
+
     s = pickle.dumps(structure)
     loaded_structure = pickle.loads(s)
 
     assert loaded_structure == structure
+    assert loaded_structure.get_chain("A") == structure.get_chain("A")
+    assert loaded_structure.get_chain("A").get_residue(0) == structure.get_chain("A").get_residue(0)
+    assert loaded_structure.get_chain("A").get_residue(0).amino_acid == structure.get_chain("A").get_residue(0).amino_acid
+    assert loaded_structure.get_chain("A").get_residue(0).atoms[0] == structure.get_chain("A").get_residue(0).atoms[0]
+
+
+def test_serialization_fork():
+
+    structure = _get_structure("tests/data/pdb/101M/101M.pdb")
+
+    s = _ForkingPickler.dumps(structure)
+    loaded_structure = _ForkingPickler.loads(s)
+
+    assert loaded_structure == structure
+    assert loaded_structure.get_chain("A") == structure.get_chain("A")
+    assert loaded_structure.get_chain("A").get_residue(0) == structure.get_chain("A").get_residue(0)
+    assert loaded_structure.get_chain("A").get_residue(0).amino_acid == structure.get_chain("A").get_residue(0).amino_acid
+    assert loaded_structure.get_chain("A").get_residue(0).atoms[0] == structure.get_chain("A").get_residue(0).atoms[0]
