2066844cd4b12511c100d9e17e798e1e3de53fd2
Coos Baakman <cbaakman@radboudumc.nl>
2023-02-06 11:38:38 +0100

    made a test to compare with the old output

--- tests/utils/test_grid.py
+++ tests/utils/test_grid.py
@@ -1,8 +1,17 @@
+from tempfile import mkstemp
+import os
+from typing import Tuple
+import logging
+
 import h5py
 import numpy as np
 
 from deeprankcore.query import ProteinProteinInterfaceAtomicQuery, ProteinProteinInterfaceResidueQuery
 from deeprankcore.utils.grid import MapMethod, GridSettings, Grid
+import deeprankcore.features.contact
+
+
+_log = logging.getLogger(__name__)
 
 
 def test_residue_grid_orientation():
@@ -99,3 +108,72 @@ def test_atomic_grid_orientation():
 
     assert grid.zs.shape == target_zs.shape
     assert np.all(np.abs(grid.zs - target_zs) < coord_error_margin), f"\n{grid.zs} != \n{target_zs}"
+
+
+def _inflate(index: np.ndarray, value: np.ndarray, shape: Tuple[int]):
+
+    size = 1
+    for dim in shape:
+        size *= dim
+
+    data = np.zeros(size)
+    data[index] = value[:, 0]
+
+    return data.reshape(shape)
+
+
+def test_grid_features():
+    "Check that grid mapped features produce output that makes sense"
+
+    error_margin = 0.001
+
+    with h5py.File("tests/data/hdf5/original-deeprank-1ak4.hdf5", 'r') as data_file:
+
+        original_chain1_electrostatic = _inflate(data_file["1AK4/mapped_features/Feature_ind/coulomb_chain1/index"][:],
+                                                 data_file["1AK4/mapped_features/Feature_ind/coulomb_chain1/value"][:],
+                                                 (10, 10, 10))
+
+        original_chain2_electrostatic = _inflate(data_file["1AK4/mapped_features/Feature_ind/coulomb_chain2/index"][:],
+                                                 data_file["1AK4/mapped_features/Feature_ind/coulomb_chain2/value"][:],
+                                                 (10, 10, 10))
+
+        original_chain1_vanderwaals = _inflate(data_file["1AK4/mapped_features/Feature_ind/vdwaals_chain1/index"][:],
+                                               data_file["1AK4/mapped_features/Feature_ind/vdwaals_chain1/value"][:],
+                                               (10, 10, 10))
+
+        original_chain2_vanderwaals = _inflate(data_file["1AK4/mapped_features/Feature_ind/vdwaals_chain2/index"][:],
+                                               data_file["1AK4/mapped_features/Feature_ind/vdwaals_chain2/value"][:],
+                                               (10, 10, 10))
+
+    pdb_path = "tests/data/pdb/1ak4/1ak4.pdb"
+
+    query = ProteinProteinInterfaceAtomicQuery(pdb_path, "C", "D",
+                                               distance_cutoff=8.5)
+
+    graph = query.build([deeprankcore.features.contact])
+
+    map_method = MapMethod.FAST_GAUSSIAN
+    grid_settings = GridSettings([10, 10, 10], [30.0, 30.0, 30.0])
+
+    hdf5_file, hdf5_path = mkstemp()
+    os.close(hdf5_file)
+
+    try:
+        graph.write_as_grid_to_hdf5(hdf5_path, grid_settings, map_method)
+
+        with h5py.File(hdf5_path, 'r') as data_file:
+
+            entry_name = list(data_file.keys())[0]
+
+            _log.debug(f"entry is {entry_name}")
+
+            electrostatic_data = data_file[entry_name]["mapped_features/electrostatic/value"][:]
+            vanderwaals_data = data_file[entry_name]["mapped_features/vanderwaals/value"][:]
+    finally:
+        os.remove(hdf5_path)
+
+    assert np.all(np.abs(original_chain1_electrostatic + original_chain2_electrostatic - electrostatic_data) < error_margin), \
+            f"difference is {np.max(np.abs(original_chain1_electrostatic + original_chain2_electrostatic - electrostatic_data))}"
+    assert np.all(np.abs(original_chain1_vanderwaals + original_chain2_vanderwaals - vanderwaals_data) < error_margin), \
+            f"difference is {np.max(np.abs(original_chain1_vanderwaals + original_chain2_vanderwaals - vanderwaals_data))}"
+
