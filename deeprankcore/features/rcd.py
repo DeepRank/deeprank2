c3668e904dd2d135f2fb8b764a22e9535a6259c4
DaniBodor <d.bodor@esciencecenter.nl>
2023-02-15 21:07:11 +0100

    create function to count all contact residues

--- deeprankcore/features/rcd.py
+++ deeprankcore/features/rcd.py
@@ -7,9 +7,59 @@ from deeprankcore.molstruct.residue import Residue
 from deeprankcore.molstruct.atom import Atom
 from deeprankcore.domain import nodestorage as Nfeat
 
+def count_residue_contacts(pdb_path: str, cutoff: float, chain1: str = 'A', chain2: str = 'B') -> dict:
+    """Count total number of close contact residues and contacts of specific Polarity.
 
-def count_neighbours(residue):
-    pass
+    Args:
+        pdb_path (str): path to pdb file to read molecular information from
+        cutoff (float): cutoff distance to be considered a close contact
+        chain1 (str, optional): name of first chain from pdb file to consider. Defaults to 'A'.
+        chain2 (str, optional): name of second chain from pdb file to consider. Defaults to 'B'.
+
+    Returns:
+        dict: keys are each residue; items are _ContactDensity objects, which containing all contact density information 
+    """
+
+    sql = pdb2sql.interface(pdb_path)    
+    pdb2sql_contacts = sql.get_contact_residues(
+        cutoff=cutoff, 
+        chain1=chain1, chain2=chain2,
+        return_contact_pairs=True
+    )
+    
+    residue_contacts = {}
+
+    for chain1_res, chain2_residues in pdb2sql_contacts.items():
+        aa1_code = chain1_res[2]
+        try:
+            aa1 = [amino_acid for amino_acid in amino_acids if amino_acid.three_letter_code == aa1_code][0]        
+        except IndexError:
+            continue  # skip keys that are not an amino acid
+
+        # add chain1_res to residue_contact dict
+        residue_contacts[chain1_res] = _ContactDensity(chain1_res)
+        
+        for chain2_res in chain2_residues:
+            aa2_code = chain2_res[2]
+            try:
+                aa2 = [amino_acid for amino_acid in amino_acids if amino_acid.three_letter_code == aa2_code][0]        
+            except IndexError:
+                continue  # skip keys that are not an amino acid
+            
+            # populate densities and connections for chain1_res
+            residue_contacts[chain1_res].densities['total'] += 1
+            residue_contacts[chain1_res].densities[aa2.polarity] += 1
+            residue_contacts[chain1_res].connections[aa2.polarity].append(chain2_res)
+            
+            # add chain2_res to residue_contact dict if it doesn't exist yet
+            if chain2_res not in residue_contacts:
+                residue_contacts[chain2_res] = _ContactDensity(chain2_res)
+            # populate densities and connections for chain2_res
+            residue_contacts[chain2_res].densities['total'] += 1
+            residue_contacts[chain2_res].densities[aa1.polarity] += 1
+            residue_contacts[chain2_res].connections[aa1.polarity].append(chain1_res)
+    
+    return residue_contacts
 
 
 def add_features(
