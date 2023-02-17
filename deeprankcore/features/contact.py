9436cd1629d9f002a8ebf32cb85baa411d9a94bc
Joyce <joyceljy6256@gmail.com>
2023-02-17 16:04:30 +0100

    Fix docstring in feature folder

--- deeprankcore/features/contact.py
+++ deeprankcore/features/contact.py
@@ -14,10 +14,10 @@ _log = logging.getLogger(__name__)
 MAX_COVALENT_DISTANCE = 2.1
 
 def _get_coulomb_potentials(atoms: List[Atom], distances: np.ndarray) -> np.ndarray:
-    """ 
-        Calculate Coulomb potentials between between all Atoms in atom.
-        Warning: there's no distance cutoff here. The radius of influence is assumed to infinite.
-            However, the potential tends to 0 at large distance.
+    """ Calculate Coulomb potentials between between all Atoms in atom.
+
+    Warning: there's no distance cutoff here. The radius of influence is assumed to infinite.
+    However, the potential tends to 0 at large distance.
     """
 
     EPSILON0 = 1.0
@@ -29,8 +29,9 @@ def _get_coulomb_potentials(atoms: List[Atom], distances: np.ndarray) -> np.ndar
 
 def _get_lennard_jones_potentials(atoms: List[Atom], distances: np.ndarray) -> np.ndarray:
     """ Calculate Lennard-Jones potentials between all Atoms in atom.
-        Warning: there's no distance cutoff here. The radius of influence is assumed to infinite.
-            However, the potential tends to 0 at large distance.
+
+    Warning: there's no distance cutoff here. The radius of influence is assumed to infinite.
+    However, the potential tends to 0 at large distance.
     """
 
     # calculate intra potentials
