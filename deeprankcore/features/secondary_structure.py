8dea783bd1ae897560f15258775f126937c7735d
Daniel Rademaker <40323363+DTRademaker@users.noreply.github.com>
2023-03-16 18:14:08 +0100

    Update secondary_structure.py
    updated to work with current deeprank logic. Did not know the nodes in graph.nodes are completely randomly ordered for some reason, had to add some code to link the correct info to the correct aminoacids again

--- deeprankcore/features/secondary_structure.py
+++ deeprankcore/features/secondary_structure.py
@@ -7,88 +7,99 @@ import numpy
 import os	
 
 
-def dssp(
-	pdb_path: str,):
-	"""
-	Calculates DSSP features for a given PDB structure.
-	
-	Args:
-		pdb_path (str): Path to the PDB file.
+import os
+import numpy as np
 
-	Returns:
-		str: A string representing the secondary structure features.
-	"""
-	output = os.popen('dssp -i %s' % pdb_path).read()
-	output = output[output.index('  #  RESIDUE'):].split('\n')[1:-1]
-	secs   = ''.join([line[16]for line in output if line[13]!='!'])
-	secs = secs.replace('B','E').replace('G', 'H').replace('I', 'H').replace('I', 'H').replace('S', 'C').replace(' ', 'C').replace('T', 'C')
-	return secs
+def dssp(pdb_path: str):
+    """
+    Process the output of the DSSP program to extract secondary structure information.
+    
+    Args:
+        pdb_path (str): The file path of the PDB file to be processed.
+    
+    Returns:
+        dict: A dictionary containing secondary structure information for each chain and residue.
+    """
+    
+    # Execute DSSP and read the output
+    dssp_output = os.popen('dssp -i %s' % pdb_path).read()
+    
+    # Extract relevant lines from the output
+    dssp_lines = dssp_output[dssp_output.index('  #  RESIDUE'):].split('\n')[1:-1]
+    
+    # Extract secondary structure features and replace codes for readability
+    sec_structure_features = ''.join([line[16] for line in dssp_lines if line[13] != '!'])
+    sec_structure_features = (sec_structure_features.replace('B', 'E')
+                                                    .replace('G', 'H')
+                                                    .replace('I', 'H')
+                                                    .replace('S', 'C')
+                                                    .replace(' ', 'C')
+                                                    .replace('T', 'C'))
+
+    # Extract residue numbers and chain identifiers
+    residue_numbers = [int(line[5:10]) for line in dssp_lines if line[13] != '!']
+    chain_ids = [line[11] for line in dssp_lines if line[13] != '!']
+    
+    # Initialize dictionary to store secondary structure information
+    sec_structure_dict = {}
+    for chain in set(chain_ids):
+        sec_structure_dict[chain] = {}
+    
+    # Sanity check: Ensure equal lengths of chain_ids, residue_numbers, and sec_structure_features
+    assert len(chain_ids) == len(residue_numbers) == len(sec_structure_features)
+    
+    # Create one-hot encoding for secondary structure features
+    one_hot_encoded_features = np.zeros((len(sec_structure_features), 3))
+    for ind in range(len(sec_structure_features)):
+        one_hot_encoded_features[ind]['HEC'.index(sec_structure_features[ind])] = 1
+
+    # Populate the dictionary with one-hot encoded secondary structure information
+    for i in range(len(chain_ids)):
+        sec_structure_dict[chain_ids[i]][residue_numbers[i]] = one_hot_encoded_features[i]
+    
+    return sec_structure_dict
 
 
 
-def sec_struc_id(sec_structure_features: str):
-	"""
-	Defines the secondary structure elements features.
-	
-	Args:
-		sec_strucutre_features (str): A string representing the secondary structure features.
+def add_features(pdb_path: str, graph: Graph):
+    """
+    Add secondary structure features to the nodes of a graph.
 
-	Returns:
-		list: A list of secondary structure element IDs.
-	"""
+    Args:
+        pdb_path (str): The file path of the PDB file to be processed.
+        graph (Graph): A Graph object representing the protein structure.
+    """
 
-	element_id = 0
-	sec_struc_ids = [0]
-	for i in range(1, len(sec_structure_features)):
-		if sec_structure_features[i] != sec_structure_features[i-1]:
-			element_id += 1
-		sec_struc_ids.append(element_id)
-	return sec_struc_ids
+    # Get the secondary structure features from the DSSP output
+    sec_structure_features = dssp(pdb_path)
+
+    # Iterate through the nodes in the graph
+    for node in graph.nodes:
+
+        # Check if the node represents a Residue object
+        if isinstance(node.id, Residue):
+            residue = node.id
+
+        # Check if the node represents an Atom object
+        elif isinstance(node.id, Atom):
+            atom = node.id
+            residue = atom.residue
+
+        # Raise an error if the node type is unexpected
+        else:
+            raise TypeError(f"Unexpected node type: {type(node.id)}")
+
+        # Get the chain ID and residue position from the node
+        chain_id = node.id.chain.id
+        residue_position = node.id.number
+
+        # Add the secondary structure feature to the node
+        node.features['ss'] = sec_structure_features[chain_id][residue_position]
+
+        # Print the residue position, chain ID, and secondary structure feature
+        print(residue_position, chain_id, node.features['ss'])
 
 
-
-def add_features_for_residues(
-	pdb_path: str, graph: Graph,):
-	"""
-	Adds node features to the graph based on the protein structure.
-
-	Args:
-		pdb_path (str): The path to the PDB file for the protein structure.
-		graph (Graph): The graph to which node features will be added to.
-	"""
-	
-	secs_features = dssp(pdb_path)
-	secStrucids = sec_struc_id(secs_features)
-	
-	one_hot = numpy.zeros((len(secs_features), 3))
-	for ind in range(len(secs_features)):
-		one_hot[ind]['HEC'.index(secs_features[ind])] = 1
-	
-	for ind, node in enumerate(graph.nodes):
-		if isinstance(node.id, Residue):
-			residue = node.id
-
-		elif isinstance(node.id, Atom):
-			atom = node.id
-			residue = atom.residue
-		else:
-			raise TypeError(f"Unexpected node type: {type(node.id)}") 
-			
-		#node.features[FEATURENAME_AMINOACID] = residue.amino_acid.onehot
-		node.features['SecStruc'] = one_hot[ind]
-		node.features['SecStrucId'] = secStrucids[ind]
-		node.features['NodeNmb'] = ind
-		
-					
-def add_features(pdb_path: str, graph: Graph, *args, **kwargs):
-	"""
-	Adds features for both residues and atoms in the graph.
-
-	Args:
-		pdb_path (str): The path to the PDB file for the protein structure.
-		graph (Graph): The graph to which features will be added.
-	"""
-	add_features_for_residues(pdb_path, graph)
 
 
 
