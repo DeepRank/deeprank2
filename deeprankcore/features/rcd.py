1a6bf5998ab15c70d0a7b8539ca479bc1f796edc
DaniBodor <d.bodor@esciencecenter.nl>
2023-03-17 02:29:01 +0100

    fix rcd cutoff distance

--- deeprankcore/features/rcd.py
+++ deeprankcore/features/rcd.py
@@ -39,7 +39,7 @@ class _ContactDensity:
         self.connections['all'] = []
 
 
-def count_residue_contacts(pdb_path: str, chains: List[str], cutoff: float) -> Dict[str, _ContactDensity]:
+def count_residue_contacts(pdb_path: str, chains: List[str], cutoff: float = 5.5) -> Dict[str, _ContactDensity]:
     """Count total number of close contact residues and contacts of specific Polarity.
 
     Args:
@@ -105,8 +105,7 @@ def add_features(
     single_amino_acid_variant = None):
     
     if not single_amino_acid_variant: # VariantQueries do not use this feature
-        chains = graph.get_all_chains()
-        residue_contacts = count_residue_contacts(pdb_path, chains, graph.cutoff_distance)
+        residue_contacts = count_residue_contacts(pdb_path, graph.get_all_chains())
         
         noncontact_residues = 0
         for node in graph.nodes:
