a60d2ab4ce09c9105cced4f35c8eb04ac8c4b051
DaniBodor <d.bodor@esciencecenter.nl>
2023-03-18 23:49:52 +0100

    use init in exposure test

--- tests/features/test_exposure.py
+++ tests/features/test_exposure.py
@@ -1,36 +1,31 @@
 import numpy as np
-from pdb2sql import pdb2sql
-from deeprankcore.features.exposure import add_features
-from deeprankcore.utils.graph import build_residue_graph
-from deeprankcore.utils.buildgraph import get_structure, get_residue_contact_pairs
+from . import build_testgraph
+from deeprankcore.utils.graph import Graph
 from deeprankcore.domain import nodestorage as Nfeat
+from deeprankcore.features.exposure import add_features
 
 
-def test_add_features():
-    pdb_path = "tests/data/pdb/1ATN/1ATN_1w.pdb"
-
-    pdb = pdb2sql(pdb_path)
-    try:
-        structure = get_structure(pdb, "1ATN_1w")
-    finally:
-        pdb._close() # pylint: disable=protected-access
-
-    residues = set([])
-    for residue1, residue2 in get_residue_contact_pairs(
-        pdb_path, structure, "A", "B", 8.5
-    ):
-        residues.add(residue1)
-        residues.add(residue2)
-    residues = list(residues)
-
-    graph = build_residue_graph(residues, "1ATN-1w", 8.5)
-
-    add_features(pdb_path, graph)
-
+def _run_assertions(graph: Graph):
     assert np.any(
         node.features[Nfeat.HSE] != 0.0 for node in graph.nodes
-    )
+    ), 'hse'
 
     assert np.any(
         node.features[Nfeat.RESDEPTH] != 0.0 for node in graph.nodes
-    )
+    ), 'resdepth'
+
+
+def test_residue_features():
+    pdb_path = "tests/data/pdb/1ATN/1ATN_1w.pdb"
+    graph = build_testgraph(pdb_path, 'residue', 8.5)
+
+    add_features(pdb_path, graph)
+    _run_assertions(graph)
+
+def test_atom_features():
+    pdb_path = "tests/data/pdb/1ATN/1ATN_1w.pdb"
+    graph = build_testgraph(pdb_path, 'residue', 8.5)
+
+    add_features(pdb_path, graph)
+    _run_assertions(graph)
+  
