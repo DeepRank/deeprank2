de4592d5b0bef9ac9bc4f3737d37d4d14e85e32c
DaniBodor <d.bodor@esciencecenter.nl>
2023-03-18 22:21:38 +0100

    conservation features

--- deeprankcore/features/conservation.py
+++ deeprankcore/features/conservation.py
@@ -18,7 +18,6 @@ def add_features( # pylint: disable=unused-argument
     for node in graph.nodes:
         if isinstance(node.id, Residue):
             residue = node.id
-
         elif isinstance(node.id, Atom):
             atom = node.id
             residue = atom.residue
@@ -26,23 +25,22 @@ def add_features( # pylint: disable=unused-argument
             raise TypeError(f"Unexpected node type: {type(node.id)}")
 
         pssm_row = residue.get_pssm()
-
         profile = np.array([pssm_row.get_conservation(amino_acid)
                                for amino_acid in profile_amino_acid_order])
 
         node.features[Nfeat.PSSM] = profile
         node.features[Nfeat.INFOCONTENT] = pssm_row.information_content
 
-        if single_amino_acid_variant is not None:
 
+        if single_amino_acid_variant is not None:
+            
             if residue == single_amino_acid_variant.residue:
                 # only the variant residue can have a variant and wildtype amino acid
-
                 conservation_wildtype = pssm_row.get_conservation(single_amino_acid_variant.wildtype_amino_acid)
                 conservation_variant = pssm_row.get_conservation(single_amino_acid_variant.variant_amino_acid)
-
                 node.features[Nfeat.CONSERVATION] = conservation_wildtype
                 node.features[Nfeat.DIFFCONSERVATION] = conservation_variant - conservation_wildtype
+
             else:
                 # all nodes must have the same features, so set them to zero here
                 node.features[Nfeat.CONSERVATION] = 0.0
