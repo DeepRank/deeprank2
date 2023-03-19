4a4023282b7a6fd5c30d6712a234cdf3ebb0456b
DaniBodor <d.bodor@esciencecenter.nl>
2023-03-19 03:23:52 +0100

    update init

--- tests/features/__init__.py
+++ tests/features/__init__.py
@@ -3,7 +3,6 @@ from typing import Optional
 from pdb2sql import pdb2sql
 from deeprankcore.utils.graph import Graph, build_atomic_graph, build_residue_graph
 from deeprankcore.utils.buildgraph import get_structure, get_residue_contact_pairs, get_surrounding_residues
-from deeprankcore.molstruct.atom import Atom
 from deeprankcore.molstruct.residue import Residue
 from deeprankcore.molstruct.structure import PDBStructure, Chain
 
@@ -16,7 +15,12 @@ def _get_residue(chain: Chain, number: int) -> Residue:
 
 
 
-def build_testgraph(pdb_path: str, cutoff: float, detail: str, central_res: Optional[int] = None) -> Graph:
+def build_testgraph(
+    pdb_path: str, 
+    cutoff: float, 
+    detail: str, 
+    central_res: Optional[int] = None, 
+    ) -> Graph:
 
 
     pdb = pdb2sql(pdb_path)
@@ -45,7 +49,7 @@ def build_testgraph(pdb_path: str, cutoff: float, detail: str, central_res: Opti
         if detail == 'residue':
             return build_residue_graph(list(nodes), structure.id, cutoff)
         elif detail == 'atom':
-            return build_atomic_graph(list(nodes), structure.id, cutoff)                
+            return build_atomic_graph(list(nodes), structure.id, cutoff)
         else:
             raise TypeError('detail must be "atom" or "residue"')
 
@@ -53,11 +57,9 @@ def build_testgraph(pdb_path: str, cutoff: float, detail: str, central_res: Opti
         residue = _get_residue(structure.chains[0], 108)
         surrounding_residues = list(get_surrounding_residues(structure, residue, cutoff))
         if detail == 'residue':
-            return build_residue_graph(surrounding_residues, structure.id, cutoff)        
+            return build_residue_graph(surrounding_residues, structure.id, cutoff)
         elif detail == 'atom':
-            atoms = [atom for residue in surrounding_residues for atom in residue.atoms]
-            return build_atomic_graph(atoms, structure.id, cutoff)     
+            atoms = set([atom for residue in surrounding_residues for atom in residue.atoms])
+            return build_atomic_graph(list(atoms), structure.id, cutoff)
         else:
             raise TypeError('detail must be "atom" or "residue"')
-                   
-
