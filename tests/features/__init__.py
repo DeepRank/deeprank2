350e85c3f974d133697ca553a1da775c4ec3473c
DaniBodor <d.bodor@esciencecenter.nl>
2023-03-19 01:11:52 +0100

    allow single chain graphs in init

--- tests/features/__init__.py
+++ tests/features/__init__.py
@@ -1,54 +1,55 @@
 import os
+from typing import Optional
 from pdb2sql import pdb2sql
 from deeprankcore.utils.graph import Graph, build_atomic_graph, build_residue_graph
-from deeprankcore.utils.buildgraph import get_structure, get_residue_contact_pairs
+from deeprankcore.utils.buildgraph import get_structure, get_residue_contact_pairs, get_surrounding_residues
 from deeprankcore.molstruct.atom import Atom
 from deeprankcore.molstruct.residue import Residue
-from deeprankcore.molstruct.structure import PDBStructure
+from deeprankcore.molstruct.structure import PDBStructure, Chain
 
 
-def build_testgraph(pdb_path: str, detail: str, cutoff: float) -> Graph:
-    """_summary_
+def _get_residue(chain: Chain, number: int) -> Residue:
+    for residue in chain.residues:
+        if residue.number == number:
+            return residue
+    raise ValueError(f"Not found: {number}")
 
-    Args:
-        pdb_path (str): _description_
-        detail (str): _description_
-        cutoff (float): _description_
 
-    Raises:
-        TypeError: _description_
 
-    Returns:
-        Graph: _description_
-    """
+def build_testgraph(pdb_path: str, cutoff: float, detail: str, central_res: Optional[int] = None) -> Graph:
+
 
     pdb = pdb2sql(pdb_path)
-    structure = get_structure(pdb, os.path.splitext(pdb_path)[0])
+    try:
+        structure: PDBStructure = get_structure(pdb, os.path.splitext(pdb_path)[0])
+    finally:
+        pdb._close() # pylint: disable=protected-access
 
-    nodes = set([])
-    for residue1, residue2 in get_residue_contact_pairs(
-        pdb_path, structure, 
-        structure.chains[0].id, structure.chains[1].id, 
-        cutoff
-    ):
-        if detail == 'residue':
-            nodes.add(residue1)
-            nodes.add(residue2)
+    if not central_res:
+        nodes = set([])
+        for residue1, residue2 in get_residue_contact_pairs(
+            pdb_path, structure, 
+            structure.chains[0].id, structure.chains[1].id, 
+            cutoff
+        ):
+            if detail == 'residue':
+                nodes.add(residue1)
+                nodes.add(residue2)
+                return build_residue_graph(list(nodes), structure.id, cutoff)
+            
+            elif detail == 'atom':
+                for atom in residue1.atoms:
+                    nodes.add(atom)
+                for atom in residue2.atoms:
+                    nodes.add(atom)
+                return build_atomic_graph(list(nodes), structure.id, cutoff)
 
-        elif detail == 'atom':
-            for atom in residue1.atoms:
-                nodes.add(atom)
-            for atom in residue2.atoms:
-                nodes.add(atom)
-
-    nodes = list(nodes)
-
-
-    if detail == 'residue':
-        return build_residue_graph(nodes, structure.id, cutoff)
-
-    elif detail == 'atom':
-        return build_atomic_graph(nodes, structure.id, cutoff)
+            else:
+                raise TypeError('detail must be "atom" or "residue"')
 
     else:
-        raise TypeError('detail must be "atom" or "residue"')
+        if detail == 'residue':
+            residue = _get_residue(structure.chains[0], 108)
+            nodes = get_surrounding_residues(structure, residue, cutoff)
+            return build_residue_graph(list(nodes), structure.id, cutoff)
+
