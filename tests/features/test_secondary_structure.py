2a2881184ab4f10ec7105c5964b37605b6197063
DaniBodor <d.bodor@esciencecenter.nl>
2023-03-23 23:34:13 +0100

    use classification function in test

--- tests/features/test_secondary_structure.py
+++ tests/features/test_secondary_structure.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from deeprankcore.domain import nodestorage as Nfeat
 from deeprankcore.features.secondary_structure import (SecondarySctructure,
+                                                       _classify_secstructure,
                                                        add_features)
 from deeprankcore.utils.graph import Graph
 
@@ -16,18 +17,27 @@ def _run_assertions(graph: Graph, node_info_list: list):
 
     # check ground truth examples
     residues = [
-        (90, 'D', SecondarySctructure.COIL.onehot),
-        (113, 'C', SecondarySctructure.STRAND.onehot),
-        (121, 'C', SecondarySctructure.HELIX.onehot),
+        (90, 'D', ' ', SecondarySctructure.COIL),
+        (89, 'D', 'S', SecondarySctructure.COIL),
+        (69, 'C', 'T', SecondarySctructure.COIL),
+        (108, 'C', 'B', SecondarySctructure.STRAND),
+        (113, 'C', 'E', SecondarySctructure.STRAND),
+        (121, 'C', 'G', SecondarySctructure.HELIX),
+        (113, 'D', 'H', SecondarySctructure.HELIX),
+        #(0, '', 'I', SecondarySctructure.HELIX),
     ]
-
+    
     for res in residues:
         node_list = [node_info for node_info in node_info_list if (node_info[0] == res[0] and node_info[1] == res[1])]
         assert len(node_list) > 0, f'no nodes detected in {res[1]} {res[0]}'
         assert np.all(
-            [np.array_equal(node_info[2], res[2])
+            [np.array_equal(node_info[2], _classify_secstructure(res[2]).onehot)
                 for node_info in node_list]
-        ), f'Ground truth examples: res {res[1]} {res[0]} is not {res[2]}'
+        ), f'Ground truth examples: res {res[1]} {res[0]} is not {(res[2])}.'
+        assert np.all(
+            [np.array_equal(node_info[2], res[3].onehot)
+                for node_info in node_list]
+        ), f'Ground truth examples: res {res[1]} {res[0]} is not {res[3]}.'
 
 
     # check entire DSSP file
@@ -64,7 +74,7 @@ def test_secondary_structure_residue():
 
 def test_secondary_structure_atom():
     pdb_path = "tests/data/pdb/1ak4/1ak4.pdb"
-    graph = build_testgraph(pdb_path, 4.5, 'atom')
+    graph = build_testgraph(pdb_path, 8.5, 'atom')
     add_features(pdb_path, graph)
     
     # Create a list of node information (residue number, chain ID, and secondary structure features)
