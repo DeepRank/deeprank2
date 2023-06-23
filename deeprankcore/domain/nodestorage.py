a0cfbef11681dc118a8ffc7a616bb45238077843
DanLep97 <danycmoilol@gmail.com>
2023-06-23 08:36:54 +0000

    added TORSIONANGLE node_feature

--- deeprankcore/domain/nodestorage.py
+++ deeprankcore/domain/nodestorage.py
@@ -20,7 +20,7 @@ RESMASS = "res_mass"
 RESPI = "res_pI"
 HBDONORS = "hb_donors" # int; former FEATURENAME_HYDROGENBONDDONORS
 HBACCEPTORS = "hb_acceptors"# int; former FEATURENAME_HYDROGENBONDACCEPTORS
-TORSIONANGLES = "torsion_angles"
+TORSIONANGLE = "torsion_angles"
 
 ## variant residue features
 VARIANTRES = "variant_res" # AminoAcid object; former FEATURENAME_VARIANTAMINOACID
