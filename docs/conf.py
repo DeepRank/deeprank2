73f2593ee1bfc0bb0dc493a5d0d183fcd3477ea2
gcroci2 <crocioni.giulia@gmail.com>
2023-08-31 14:06:37 +0200

    revert to read_configuration

--- docs/conf.py
+++ docs/conf.py
@@ -20,9 +20,9 @@
 import os
 import sys
 
-from setuptools.config import setupcfg
+from setuptools.config import read_configuration
 
-CONFIG = setupcfg('./../setup.cfg')
+CONFIG = read_configuration('./../setup.cfg')
 
 
 autodoc_mock_imports = [
@@ -96,7 +96,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'deeprank2'
-author = ''  # TODO: Fill author of documentation
+author = 'Sven van der Burg, Giulia Crocioni, Dani Bodor'
 copyright = f"2022, {author}"
 
 # The version info for the project you're documenting, acts as replacement for
@@ -113,7 +113,7 @@ release = version
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
