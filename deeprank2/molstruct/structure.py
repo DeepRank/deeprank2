8249bd600b35c1d9990324a4221e4ef92f4f3840
gcroci2 <crocioni.giulia@gmail.com>
2023-08-31 10:55:11 +0200

    rename deeprankcore to deeprank2

---
+++ deeprank2/molstruct/structure.py
@@ -0,0 +1,120 @@
+from typing import Optional
+
+from deeprank2.utils.pssmdata import PssmRow
+
+
+class PDBStructure:
+    "represents one entire pdb structure"
+
+    def __init__(self, id_: Optional[str] = None):
+        """
+        Args:
+            id_ (str, optional): An unique identifier for this structure, can be the pdb accession code.
+                Defaults to None.
+        """
+        self._id = id_
+        self._chains = {}
+
+    def __eq__(self, other) -> bool:
+        return isinstance(self, type(other)) and self._id == other._id
+
+    def __hash__(self) -> hash:
+        return hash(self._id)
+
+    def __repr__(self) -> str:
+        return self._id
+
+    def has_chain(self, chain_id: str) -> bool:
+        return chain_id in self._chains
+
+    def get_chain(self, chain_id: str):
+        return self._chains[chain_id]
+
+    def add_chain(self, chain):
+        if chain.id in self._chains:
+            raise ValueError(f"duplicate chain: {chain.id}")
+
+        self._chains[chain.id] = chain
+
+    @property
+    def chains(self):
+        return list(self._chains.values())
+
+    def get_atoms(self):
+        "shortcut to list all atoms in this structure"
+        atoms = []
+        for chain in self._chains.values():
+            for residue in chain.residues:
+                atoms.extend(residue.atoms)
+
+        return atoms
+
+    @property
+    def id(self) -> str:
+        return self._id
+
+
+class Chain:
+    "represents one pdb chain"
+
+    def __init__(self, model: PDBStructure, id_: Optional[str]):
+        """
+        Args:
+            model (:class:`PDBStructure`): The model that this chain is part of.
+            id_ (str): The pdb identifier of this chain.
+        """
+
+        self._model = model
+        self._id = id_
+        self._residues = {}
+        self._pssm = None  # pssm is per chain
+
+    @property
+    def model(self):
+        return self._model
+
+    @property
+    def pssm(self) -> PssmRow:
+        return self._pssm
+
+    @pssm.setter
+    def pssm(self, pssm: PssmRow):
+        self._pssm = pssm
+
+    def add_residue(self, residue):
+        self._residues[(residue.number, residue.insertion_code)] = residue
+
+    def has_residue(self, residue_number: int, insertion_code: Optional[str] = None) -> bool:
+        return (residue_number, insertion_code) in self._residues
+
+    def get_residue(self, residue_number: int, insertion_code: Optional[str] = None):
+        return self._residues[(residue_number, insertion_code)]
+
+    @property
+    def id(self) -> str:
+        return self._id
+
+    @property
+    def residues(self):
+        return list(self._residues.values())
+
+    def get_atoms(self):
+        """Shortcut to list all atoms in this chain."""
+        atoms = []
+        for residue in self._residues.values():
+            atoms.extend(residue.atoms)
+
+        return atoms
+
+    def __eq__(self, other) -> bool:
+        return (
+            isinstance(self, type(other))
+            and self._model == other._model
+            and self._id == other._id
+        )
+
+    def __hash__(self) -> hash:
+        return hash(self._id)
+
+    def __repr__(self) -> str:
+        return f"{self._model} {self._id}"
---
+++ deeprank2/utils/parsing/pssm.py
@@ -0,0 +1,58 @@
+from typing import TextIO
+
+from deeprank2.domain.aminoacidlist import amino_acids
+from deeprank2.molstruct.residue import Residue
+from deeprank2.molstruct.structure import Chain
+from deeprank2.utils.pssmdata import PssmRow, PssmTable
+
+
+def parse_pssm(file_: TextIO, chain: Chain) -> PssmTable:
+    """Read the PSSM data.
+
+    Args:
+        file_ (python text file object): The pssm file.
+        chain (:class:`Chain`): The chain that the pssm file represents, residues from this chain must match the pssm file.
+
+    Returns
+        PssmTable: The position-specific scoring table, parsed from the pssm file.
+    """
+
+    amino_acids_by_letter = {amino_acid.one_letter_code: amino_acid for amino_acid in amino_acids}
+    conservation_rows = {}
+
+    # Read the pssm header.
+    header = next(file_).split()
+    column_indices = {
+        column_name.strip(): index for index, column_name in enumerate(header)
+    }
+
+    for line in file_:
+        row = line.split()
+
+        # Read what amino acid the chain is supposed to have at this position.
+        amino_acid = amino_acids_by_letter[row[column_indices["pdbresn"]]]
+
+        # Some PDB files have insertion codes, find these to prevent
+        # exceptions.
+        pdb_residue_number_string = row[column_indices["pdbresi"]]
+        if pdb_residue_number_string[-1].isalpha():
+
+            pdb_residue_number = int(pdb_residue_number_string[:-1])
+            pdb_insertion_code = pdb_residue_number_string[-1]
+        else:
+            pdb_residue_number = int(pdb_residue_number_string)
+            pdb_insertion_code = None
+
+        # Build the residue, to match with the pssm row
+        residue = Residue(chain, pdb_residue_number, amino_acid, pdb_insertion_code)
+
+        # Build the pssm row
+        information_content = float(row[column_indices["IC"]])
+        conservations = {
+            amino_acid: float(row[column_indices[amino_acid.one_letter_code]])
+            for amino_acid in amino_acids
+        }
+
+        conservation_rows[residue] = PssmRow(conservations, information_content)
+
+    return PssmTable(conservation_rows)
