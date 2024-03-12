78e07af5be7b299c0702229d29a41f1ac64736a8
Dani Bodor <d.bodor@esciencecenter.nl>
2024-03-12 14:03:29 +0100

    add pruning pdb file (steps 1-3 from pdbprep)

--- deeprank2/tools/pdbprep/__init__.py
+++ deeprank2/tools/pdbprep/__init__.py
@@ -2,9 +2,12 @@
 # which is published under an Apache 2.0 licence
 
 import sys
+from collections.abc import Generator
+from typing import TextIO
 
 
-def write_pdb(new_pdb, pdbfh):
+def write_pdb(new_pdb: list, pdbfh: TextIO) -> None:
+    """Writes new pdb files."""
     try:
         _buffer = []
         _buffer_size = 5000  # write N lines at a time
@@ -26,3 +29,43 @@ def write_pdb(new_pdb, pdbfh):
     # We can close it even if it is sys.stdin
     pdbfh.close()
     sys.exit(0)
+
+
+def _prune_records(fhandle: TextIO) -> Generator[str]:
+    """Prune records before processing.
+
+    Scraps non-atomic records and records from water molecule.
+    Replaces non-standard residue names by their standard counterparts.
+    """
+    atomic_record = ("ATOM", "HETATM")  # TODO: check if we need to keep ANISOU and TER records as well?
+    water = "HOH"
+    standard_resnames = {
+        "MSE": "MET",
+        "HIP": "HIS",
+        "HIE": "HIS",
+        "HID": "HIS",
+        "HSE": "HIS",
+        "HSD": "HIS",
+    }
+
+    for record in fhandle:
+        resname = record[17:20]
+        if record.startswith(atomic_record) and resname != water:
+            standardized_resname = standard_resnames.get(resname, resname)
+            yield record[:17] + standardized_resname + record[20:]
+
+
+def pdb_prep(fhandle: TextIO) -> None:
+    """Run all steps from pdb prep repo."""
+    # step 1 - keep coordinates: removes non coordinate lines for simplicity
+    # step 2 - delresname: remove waters
+    # step 3 - rplresname: convert residue names to standard names, ex: MSE to MET
+    new_pdb = _prune_records(fhandle)
+
+    # step 4 - selaltloc: select most probable alternative location
+
+    # step 5 - fixinsert: fix inserts
+    # step 6 - sort: sort chains and resides, necessary for OpenMM
+    # step 7 - reres: renumber residues from 1
+    # step 8 - reatom: renumber atoms from 1
+    # step 9 - tidy: tidy cleans the PDB, adds TER, etc.
