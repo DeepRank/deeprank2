2181c1b5fcf33d59caf9e2398c00585500ce253a
gcroci2 <crocioni.giulia@gmail.com>
2024-07-09 16:53:26 +0200

    switch back to md

--- docs/conf.py
+++ docs/conf.py
@@ -80,7 +80,6 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "myst_parser",
-    "m2r",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
