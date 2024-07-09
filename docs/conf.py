f4abadef0b15a2c57ba7bfb5fe68eec6b1148246
gcroci2 <crocioni.giulia@gmail.com>
2024-07-09 16:47:44 +0200

    use m2r

--- docs/conf.py
+++ docs/conf.py
@@ -80,6 +80,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "myst_parser",
+    "m2r",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
