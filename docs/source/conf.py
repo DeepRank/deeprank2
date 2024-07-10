e21fbc7d12726c207eeabf4f4e1808b81f31e01f
gcroci2 <crocioni.giulia@gmail.com>
2024-07-10 15:07:26 +0200

    add module path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,6 +56,7 @@ autodoc_mock_imports = [
 
 # Add the project's root directory to sys.path
 sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../deeprank2"))
 
 # -- General configuration ------------------------------------------------
 
